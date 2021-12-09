829bbc3ced712385ed355baa136ed19f6d0e76a7
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-09 16:02:40 +0600

    coin create complete

--- tests/common/settings/TC_035_common_settings_put_request_update_no_token_test.py
+++ tests/common/settings/TC_035_common_settings_put_request_update_no_token_test.py
@@ -1,7 +1,6 @@
 import allure
 from api.common_api import Api
 from test_data.common_data.data import new_fee
-from test_data.headers import headers_with_token
 
 
 @allure.step("Verify that user can not update fee with PUT request without token")
@@ -10,4 +9,3 @@ def test_common_settings_get_no_token_request():
     result = api.put_request(new_fee)
     status_code = result['status_code']
     assert status_code == 401
-    
