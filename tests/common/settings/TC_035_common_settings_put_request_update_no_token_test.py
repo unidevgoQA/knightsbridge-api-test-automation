46b24376e9d6e967266d0796768c6348132fd78a
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-09 15:39:51 +0600

    common put fixed

---
+++ tests/common/settings/TC_035_common_settings_put_request_update_no_token_test.py
@@ -0,0 +1,13 @@
+import allure
+from api.common_api import Api
+from test_data.common_data.data import new_fee
+from test_data.headers import headers_with_token
+
+
+@allure.step("Verify that user can not update fee with PUT request without token")
+def test_common_settings_get_no_token_request():
+    api = Api("common/settings")
+    result = api.put_request(new_fee)
+    status_code = result['status_code']
+    assert status_code == 401
+    
