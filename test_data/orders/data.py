5ac853382d31e134db4a8f7c59b8cbd40fe72271
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-10 18:29:51 +0600

    order create complete

--- test_data/orders/data.py
+++ test_data/orders/data.py
@@ -1,7 +1,7 @@
-import uuid
+from utils.read_update_counter import read_counter
 
-new_order = {
-  "uid": str(uuid.uuid4().hex),
+new_order_data = {
+  "uid": "testuid{}".format(read_counter()),
   "coinId": "61b1d3ae18ec7f2e0be3980c",
   "quantity": 2,
   "desiredRate": 2,
