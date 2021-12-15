2f8e5fd7b7d2e9a964e2cb558c49e13f72b3152e
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-15 14:32:27 +0600

    fiat wallet update by userId

--- tests/fiat_wallet/update_by_userid/TC_187_update_fiat_wallet_by_user_id_success_test.py
+++ tests/fiat_wallet/update_by_userid/TC_187_update_fiat_wallet_by_user_id_success_test.py
@@ -4,7 +4,7 @@ from test_data.headers import admin_headers_with_token
 from test_data.fiat_wallet.data import fiat_wallet_user_id, fiat_wallet_update_data
 
 
-@allure.step("Verify retrieve fiat wallet by user id success test")
+@allure.step("Verify retrieve fiat wallet by user id admin token success test")
 def test_update_fiat_wallet_user_id():
     fiat_api = Api("fiatWallet/update/{}".format(fiat_wallet_user_id))
     fiat_api = fiat_api.put_request(payload=fiat_wallet_update_data,
