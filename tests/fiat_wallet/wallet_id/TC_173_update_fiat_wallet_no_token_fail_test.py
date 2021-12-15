b8982a2f1875d56d0dddfed6bf0f54ae1ee033c2
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-15 12:39:35 +0600

    update endpoint complete

--- tests/fiat_wallet/wallet_id/TC_173_update_fiat_wallet_no_token_fail_test.py
+++ tests/fiat_wallet/wallet_id/TC_173_update_fiat_wallet_no_token_fail_test.py
@@ -1,13 +1,12 @@
 import allure
 from api.common_api import Api
 from test_data.fiat_wallet.data import fiat_wallet_id
-from test_data.headers import admin_headers_with_token
 
 
-@allure.step("Verify that user can update fiat wallet by valid data")
+@allure.step("Verify update fiat wallet by user fail test")
 def test_update_fiat_wallet():
     endpoint = "fiatWallet/{}".format(fiat_wallet_id)
     fiat_api = Api(endpoint)
-    result = fiat_api.put_request(headers=admin_headers_with_token)
+    result = fiat_api.put_request()
     status_code = result['status_code']
-    assert status_code == 200
+    assert status_code == 401
