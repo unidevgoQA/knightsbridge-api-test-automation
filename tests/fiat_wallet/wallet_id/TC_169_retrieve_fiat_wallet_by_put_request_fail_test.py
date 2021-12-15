08600da0eb4830b755de0177bab2597fc18fc634
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-15 11:39:08 +0600

    fiat wallet retrieve complete

---
+++ tests/fiat_wallet/wallet_id/TC_169_retrieve_fiat_wallet_by_put_request_fail_test.py
@@ -0,0 +1,12 @@
+import allure
+from api.common_api import Api
+from test_data.fiat_wallet.data import fiat_wallet_id
+
+
+@allure.step("Verify retrieve fiat wallet by id put request fail test")
+def test_retrieve_fiat_wallet():
+    endpoint = "fiatWallet/{}".format(fiat_wallet_id)
+    fiat_api = Api(endpoint)
+    result = fiat_api.post_request()
+    status_code = result['status_code']
+    assert status_code == 404
