9524e307599253ceaa84040bd10c94605e14c0a2
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-20 17:13:52 +0600

    run_test update

--- test_conf/run_test.py
+++ test_conf/run_test.py
@@ -8,7 +8,7 @@ from utils.read_update_counter import read_and_update_counter
 report_folder_name = f"{read_date()}_{read_and_update_counter()}"
 # running testCases
 command = f"pytest -s --alluredir=report_allure/{report_folder_name} " \
-          f"--html=report_html/report_{report_folder_name}.html --self-contained-html tests/coupons"
+          f"--html=report_html/report_{report_folder_name}.html --self-contained-html tests"
 subprocess.run(command, shell=True)
 
 # generating report
