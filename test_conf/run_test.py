8fa1b6eb77e5535220fe878123a363560bac1856
unidevgoQA <unidevgoqa.1@gmail.com>
2021-12-21 16:57:13 +0600

    run_test update

--- test_conf/run_test.py
+++ test_conf/run_test.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.read_date import read_date
@@ -7,9 +8,9 @@ from utils.read_update_counter import read_and_update_counter
 report_folder_name = f"{read_date()}_{read_and_update_counter()}"
 # running testCases
 command = f"pytest -s --alluredir=report_allure/{report_folder_name} " \
-          f"--html=report_html/report_{report_folder_name}.html --self-contained-html tests"
-os.system(command)
+          f"--html=report_html/report_{report_folder_name}.html --self-contained-html tests/coupons/coupon_create"
+subprocess.run(command, shell=True)
 
 # generating report
 command = f"allure serve report_allure/{report_folder_name}"
-os.system(command)
+subprocess.run(command, shell=True)
